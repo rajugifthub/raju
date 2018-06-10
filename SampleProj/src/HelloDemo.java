b9dfcf421d2c66d43c39c12909220b81d13bc6af
Rajesh-pc <Rajesh-pc@DESKTOP-B27DTTE>
2018-06-11 01:14:59 +0530

    edit

--- SampleProj/src/HelloDemo.java
+++ SampleProj/src/HelloDemo.java
@@ -3,8 +3,11 @@ public class HelloDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hiiiiiiiii");
-		System.out.println("gd nyt");
+	//	System.out.println("hiiiiiiiii");
+		//System.out.println("gd nyt");
+		for(int i=0;i<=10;i++){
+			System.out.println(i);
+		}
 
 	}
 
